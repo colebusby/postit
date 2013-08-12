716917a6424f1b8fdb353d16d934a91d6f6d4aec
Cole Busby <colebusby@gmail.com>
2013-08-12 09:59:32 -0600

    creating view files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,27 @@
 class PostsController < ApplicationController
+  
+
+  def create
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+
+  end
+
+  #def show
+
+  #end
+
   def index
+      @posts = Post.all
+  end
+
+  def update
 
   end
 end
